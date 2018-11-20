d09a0174b6cb6456026d086115472d280c48da3c
javierdiplan04 <44383709+javierdiplan04@users.noreply.github.com>
2018-11-19 21:11:47 -0400

    Login Funcional

--- app/login.js
+++ app/login.js
@@ -1,4 +1,44 @@
+$(document).ready(function(){
+$("#login").click(function(){
 
+login();
+
+});
+});
+
+function login()
+{
+   // alert("Las contaseñas son diferentes2");
+   var email = $("#email").val();
+   var password = $("#password").val();
+
+   var data = {
+       email : email,
+       password: password
+   }
+
+   console.log(data);
+   
+   fetch("http://68.183.27.173:8080/login", {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+  .then((response)=>{
+    var userdata=JSON.stringify(response);
+    localStorage.setItem('Api',userdata);
+    window.location.href="http://localhost/it247/halo/index.html";
+
+  })
+  //.then(response => console.log('Success:', JSON.stringify(response)))
+  .catch(error => console.error('Error:', error));
+}
+
+
+
+/*
 function login()
 {
    var username = $("#username").val();
@@ -37,4 +77,4 @@ else{
 alert("Bienvenido "+nombre);
 }
 
-}
+} */
