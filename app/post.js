d73f7e7ddb906c3285e28d1658694ff77dd9b727
javierdiplan04 <44383709+javierdiplan04@users.noreply.github.com>
2018-12-04 10:18:09 -0400

    Agregacion de estilo, y otras nuevas funciones

--- app/post.js
+++ app/post.js
@@ -51,9 +51,10 @@ function post()
         //localStorage.setItem('Api',userdata);
          //window.location.href="http://localhost/blogapi/post.html";
          let obj=Object.keys(response).map(element=>{
-     let {title,body,userName,userEmail}=response[element];
+     let {title,body,userName,userEmail, views, likes, liked, comments,userId,id, tags, createdAt}=response[element];
+     let fecha= new Date(createdAt).toLocaleDateString("es-RD");
      //return `<h4>${title}</h4><br> <h4>${body}</h4> <br> <h4>${userName}</h4><br>`
-     return `
+     /*return `
      <table width="100%" border="0">
      <tbody>
        <tr>
@@ -81,7 +82,44 @@ function post()
        </button></td>
        </tr>
      </tbody>
-   </table>`
+   </table>`*/
+
+   return `<!--================Blog Area =================-->
+   
+   <div class="row">
+                        <div class="blog_left_sidebar">
+                       <article class="blog_style1">
+                         <div class="blog_img">
+                           <img class="img-fluid" src="img/home-blog/blog-1.jpg" alt="">
+                         </div>
+                         <div class="blog_text">
+             <div class="blog_text_inner">
+               <div class="cat">
+                 <a class="cat_btn" href="#"><i class="fas fa-tag"></i> ${tags}</a>
+                 <a href="#"><i class="fa fa-calendar" aria-hidden="true"></i> ${fecha}</a>
+                 <a href="#"><i class="fa fa-comments-o" aria-hidden="true"></i> <span id="articulo-comment-${id}"> ${comments}</a>
+                 <a href="#"><i class="fas fa-eye" aria-hidden="true"></i> <span id="articulo-view-${id}"> ${views} </span></a>
+                 <i class="fas fa-star" aria-hidden="true"></i> <span id="articulo-like-${id}">${likes} </span>
+               </div>
+               <a href="#"><h4>${title}</h4></a><a href="#"><h7>By ${userName} - ${userEmail}</h7></a>
+               <p class="category"><a onclick="ShowUser(${userId});">By: ${userName} (${userEmail})</a></p>
+               <br></br>
+               <p>${body}</p>
+               
+               <button class="btn btn-small btn-link " onclick="Like(${liked},${id});">${(liked)?'<i class="fa fa-star"></i>':'<i class="fa fa-star-o"></i>'} </button>  ${likes} Me gustas
+               <div id="ListadoComentario${id}"></div>
+         
+       </button>
+       
+             </div>
+
+           </div>
+                       </article>   
+                       </div></div>
+                       <br></br>
+       
+             
+   <!--================Blog Area =================-->`
 
 
      })
@@ -95,7 +133,162 @@ function post()
 
 (function(){
 post();
+//like();
+wsConnect();
+//comentarios();
 })();
+
+
+// Dar likes
+
+//funcion para agregar y quitar like
+function Like(like,id)
+{ 
+    var userdata=JSON.parse(localStorage.getItem('Api'));
+      
+     console.log(id,userdata);
+    fetch(`http://68.183.27.173:8080/post/${id}/like`,{
+        method: (like == true) ? 'DELETE': 'PUT',
+        headers:{
+            'Content-Type':'application/json',
+            'Authorization':`Bearer ${userdata.token}`
+
+        }
+    })
+     .then((response)=>{
+        GetlistPost();
+     }).catch(error=>console.log("Error:",error));
+
+}
+
+// fin dar likes
+
+//mostrar usuario
+
+function  ShowUser(userId)
+{   var URL =location.protocol + "//" +location.host; 
+    localStorage.setItem('userId',userId);
+    
+     window.location.href=URL+"home/Users/javierdiplantavarez/Documents/ITLA/JavaScrip/tarea2/perfil.html";
+    
+}
+//funcion para agregar y quitar like
+function Like(like,id)
+{ 
+    var userdata=JSON.parse(localStorage.getItem('Api'));
+      
+     console.log(id,userdata);
+    fetch(`http://68.183.27.173:8080/post/${id}/like`,{
+        method: (like == true) ? 'DELETE': 'PUT',
+        headers:{
+            'Content-Type':'application/json',
+            'Authorization':`Bearer ${userdata.token}`
+
+        }
+    })
+     .then((response)=>{
+        GetlistPost();
+     }).catch(error=>console.log("Error:",error));
+
+ 
+
+}
+
+//fin mostrar usuario
+
+
+
+
+//comentarios
+
+function GetlistComment(id)
+{    
+    
+      if(localStorage.getItem('Api'))
+        {   
+             var userdata=JSON.parse(localStorage.getItem('Api'));
+        
+            fetch(`http://68.183.27.173:8080/post/${id}/comment`,{
+                method:'GET',
+                headers:{
+                    'Content-Type':'application/json',
+                    'Authorization':'Bearer '+userdata.token
+
+                }
+            }).then(res=>res.json())
+             .then((response)=>{
+                console.log(response);
+                $(`#ListadoComentario${idPost}`).html("");
+               for(var i=0; i< response.length; i++)
+               {
+               var data=`  <hr>
+               <div class="header">
+               <small> <i class="fa fa-user"></i> <a onclick="ShowUser(${response[i].userId});" >By:${response[i].userName}</a></samll>  
+               </div>
+               <div class="content">
+            <div class="form-group">
+           <label>Comentario</label>
+                                                <textarea rows="1" class="form-control border-input" placeholder="Here can be your description" value="Mike">
+                                                ${response[i].body}
+                                                </textarea>
+                                            </div>
+                  
+               </div>`;
+             $(`#ListadoComentario${idPost}`).prepend(data);
+               }
+            
+                
+             })
+            .catch(error=>console.log("Error:",error));
+
+
+        }
+
+}
+
+//fin comentario
+
+
+
+// funcion websocket
+function wsConnect() {
+
+  let {token}=JSON.parse(localStorage.getItem('Api'))
+    console.log("WS- connect ", token);
+    var websocket = new WebSocket(`ws://68.183.27.173:8080/?token=${token}`);
+    websocket.onopen = function (evt) {
+        console.log(evt)
+    };
+    websocket.onclose = function (evt) {
+        console.log(evt)
+    };
+    websocket.onerror = function (evt) {
+        console.log(evt)
+    };
+
+    websocket.onmessage = function (evt) {
+        var data = JSON.parse(evt.data);
+        console.log(data);
+        switch (data.type) {
+            case "likes":
+                $('#articulo-like-' + data.postId).text(data.likes);
+                break;
+            case "view-post":
+                // TODO: cambias likes por views
+                $('#articulo-view-' + data.postId).text(data.views);
+                break;
+            case "view-comment":
+                // TODO: cambias likes por views
+                $('#articulo-comment-' + data.postId).text(data.likes);
+                break;
+
+        }
+    };
+}
+
+
+// fin funcion websocket
+
 
 
 
